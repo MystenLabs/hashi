ff033497f5a7cc12442460aa312d6af2e56dcf8e
Zhou Fang <fang.github@gmail.com>
2025-11-18 13:15:20 -0800

    docs: add TODO

--- crates/hashi/src/dkg/mod.rs
+++ crates/hashi/src/dkg/mod.rs
@@ -451,9 +451,11 @@ impl DkgManager {
             complaint: complaint.clone(),
         };
         let mut responses = Vec::new();
+        // TODO: Only need to collect t valid responses
         for signer in signers {
             // TODO: Add timeout and retries handling when adding RPC layer
             let response = p2p_channel.complain(signer, &complaint_request).await?;
+            // TODO: Add cryptographic validation of response
             responses.push(response.response);
         }
         let dealer_session_id = self.session_context.dealer_session_id(dealer);
