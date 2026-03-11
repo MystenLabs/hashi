8fab85f357f7d9fe2f465354fcd5daac86ea696f
Deepak Maram <deepak@mystenlabs.com>
2026-03-11 13:34:22 -0400

    retry more when failing

--- crates/hashi-guardian-enclave/src/lib.rs
+++ crates/hashi-guardian-enclave/src/lib.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 // TODO: Leave as consts or make them configurable?
 pub const HEARTBEAT_INTERVAL: Duration = Duration::from_mins(1);
-pub const MAX_HEARTBEAT_FAILURES: u32 = 5;
+pub const HEARTBEAT_RETRY_INTERVAL: Duration = Duration::from_secs(10);
+pub const MAX_HEARTBEAT_FAILURES_INTERVAL: Duration = Duration::from_mins(5);
 
 pub mod s3_logger; // used by the monitor
